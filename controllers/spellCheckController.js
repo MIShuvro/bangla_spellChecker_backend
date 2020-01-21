b78d724e489fb93fc82c5c53870c6b74c7828cbb
Shuvro <manjarulislam75@gmail.com>
2020-01-21 16:14:59 +0600

    set words

--- controllers/spellCheckController.js
+++ controllers/spellCheckController.js
@@ -2,8 +2,6 @@ const fs = require('fs')
 
 module.exports.spellCheck = async (req, res) => {
 
-    // // Get Dictionary
-    // const data = await fs.readFileSync('E:/10MS/banglaSpellChecker/controllers/BengaliWordList_main.txt')
     const path = require('path')
     const file = await fs.readFileSync(path.join(__dirname + '/BengaliWordList_main.txt'))
     const dictionaryWords = file.toString()
@@ -16,17 +14,17 @@ module.exports.spellCheck = async (req, res) => {
 
     const response = req.body.filter(element => dictionary.indexOf(element) == -1)
 
-    var wrongword = [];
+    var words = [];
     for (i = 0; i < response.length; i++) {
-        if (wrongword.indexOf(response[i]) === -1) {
-            wrongword.push(response[i]);
+        if (words.indexOf(response[i]) === -1) {
+            words.push(response[i]);
         }
     }
     res.status(200).json({
         "code": 200,
         "status": "OK",
         "data": {
-            wrongword
+            words
         }
 
     })
--- server.js
+++ server.js
@@ -17,26 +17,3 @@ app.use('/spellCheck', spellcheckRoute)
 app.listen(PORT, () => {
     console.log(`Server is running on port is ${PORT}`)
 })
-
-// const data = fs.readFileSync('test.txt')
-// const dictionaryWords = data.toString()
-// const dictionaryWordlist = dictionaryWords.split(' ', data.length)
-
-// var array
-// dictionaryWordlist.map(data => {
-
-//     array = data.split(/\r\n|\n|\r/)
-// })
-// const check=["যে"
-// ,"যেথা",
-// "যেথাসেথা",
-// "যেথাকার",
-// "যেগুলো",
-// "যেকোন",
-// "যেহেতু",
-// "যেতে",
-// "যেতেই"]
-// const json=JSON.stringify(array)
-// const p=JSON.parse(json)
-// console.log(p[0])
-// console.log(check[0])
